e21a66876266f40749d2718db411d9c7c88109bc
moaatazaamer <56022375+moaatazaamer@users.noreply.github.com>
2019-10-07 19:04:50 +0400

    Init
    first upload

---
+++ Contollers/customerController.js
@@ -0,0 +1,50 @@
+﻿App.controller("customerController", ["$scope", "customerService", 
+	function ($scope, customerService) {
+
+		$scope.isDisplayLogin = true;
+		$scope.selectSubTab = true;
+		$scope.getCustomerInfo = function () {
+			if (customerService) {
+				$scope.customer = customerService.dummyObj ? angular.copy(customerService.dummyObj) : {};
+				$scope.isDisplayLogin = false;
+				customerService.getSubscription().then(
+					function (response) {
+						if (response) {
+							$scope.customer.subscription = response;							
+
+							}						
+
+					});
+				customerService.getOffers().then(
+					function (response) {
+						if (response) {
+							$scope.customer.offers = response.offers;
+
+						}
+
+					});
+
+			}
+		}
+		$scope.showtab = function (tabName) {
+			initTabs();
+			switch (tabName) {
+				case 'sub':
+				default:
+					$scope.selectSubTab = true
+					break;
+				case 'offer':
+					$scope.selectOfferTab = true;
+					break;
+				case 'full':
+					$scope.selectFullTab = true;
+					break;
+
+			}
+		}
+		function initTabs() {
+			$scope.selectSubTab = false;
+			$scope.selectOfferTab = false;
+			$scope.selectFullTab = false;
+		}
+	}]);
---
+++ Services/customerService.js
@@ -0,0 +1,32 @@
+﻿App.service("customerService", ["$http",
+	function ($http) {
+		return {
+			getSubscription: getSubscription,
+			getOffers: getOffers,
+			
+		}
+		function getSubscription() {
+			var request = $http({
+				method: "GET",
+				url: "https://selfcare-service.demo.melita.com/interview/api/offers/100/subscriptions"
+			});
+			return (request.then(
+				function (response) {
+					return response.data;
+				}, function (error) {
+					return error;
+				}));
+		}
+		function getOffers() {
+			var request = $http({
+				method: "GET",
+				url: "https://selfcare-service.demo.melita.com/interview/api/offers"
+			});
+			return (request.then(
+				function (response) {
+					return response.data;
+				}, function (error) {
+					return error;
+				}));
+		}		
+	}]);
